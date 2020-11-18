c31274c70c8dd49cfcf7b663786f2626ce153648
Vifuent20 <vifuentet@gmail.com>
2020-11-18 08:44:36 +0100

    Subida del archivo P413, medio hecho

---
+++ P413_ClarasyOscuras.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class P413_ClarasyOscuras {
+
+	static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		int casos;
+		
+		int filas;
+		int columnas;
+		int blancas = 0;
+		int negras = 0;
+		double cantidad = 0;
+		double total;
+
+
+		casos = teclado.nextInt();
+
+		for(int cont=0; cont<casos; cont++) {
+
+			filas = teclado.nextInt();
+			columnas = teclado.nextInt();
+
+
+		}
+
+
+	}
+
+}
